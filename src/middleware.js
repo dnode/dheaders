a8f8815387f86a885c98fc90f736bbe01e63ec69
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-05-03 13:50:16 +0200

    Simplify the set of the headers

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,12 @@
 'use strict';
 
-let headers = [
-  ['X-Content-Type-Options', 'nosniff'],
-  ['X-Download-Options', 'noopen'],
-  ['X-Frame-Options', 'DENY'],
-  ['X-UA-Compatible', 'IE=edge,chrome=1'],
-  ['X-XSS-Protection', '1; mode=block']
-];
 module.exports = (req, res, next) => {
-  for (let header of headers) {
-    res.setHeader(header[0], header[1]);
-  }
+  res.set({
+    'X-Content-Type-Options': 'nosniff',
+    'X-Download-Options': 'noopen',
+    'X-Frame-Options': 'DENY',
+    'X-UA-Compatible': 'IE=edge,chrome=1',
+    'X-XSS-Protection': '1; mode=block'
+  });
   next();
 };
